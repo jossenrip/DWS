8d5214afbb2e991b7556cbca705420cc670e12d3
jossenrip <jossenrip@alu.edu.gva.es>
2026-09-16 20:26:35 +0200

    ejercicio 1 sin css

---
+++ ud1/ejercicios1/info_basica.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+<?php
+    $nombre = "Jose Sendra";
+    $anyo = 2005;
+
+    echo "<p>Me llamo $nombre y nací en el año $anyo.</p>"
+?>
+</body>
+</html>
